c704c313b230c4f4b1f86a2f75f7fb04bc58a5c8
nitrosx (Max Novelli) <man8@pitt.edu>
2016-12-09 11:00:05 -0500

    fixes and comments on assignment #3

--- f2016_cs8_JPD59_a3/f2016_cs8_JPD59_a3.py
+++ f2016_cs8_JPD59_a3/f2016_cs8_JPD59_a3.py
@@ -1,43 +1,61 @@
-#Open master input file. Then open files within master input file.
+#
+# MN: header with user, instructor, course info is missing
+#
+# Notes:
+# MN: try to use variables and data that you already defined.
+# MN: maybe a little bit more comments
+# MN: more attention to formatting output
+
+
+# Open master input file. Then open files within master input file.
+# MN: why not asking the user for the master list file name
 dataSources= "f2016_cs8_a3.data.txt"
 with open(dataSources) as fh:
     for line in fh:
         fh = open(dataSources, 'r')
         sourceFiles = fh.readlines()
 
-#strip end of line command from file names
+# strip end of line command from file names
 sourceFiles = [item.strip('\n') for item in sourceFiles]
 
-#Initialize data container using a dictionary (person_distances) and a list (data)
+# Initialize data container using a dictionary (person_distances) and a list (data)
 person_distances = {}
 data = []
-#set default values for max and min values
+# set default values for max and min values
 max = 0
 maxkey = 0
 min = -1
 minkey = 0
-#set counter for number of input files
+# set counter for number of input files
 input_files = 0
-#loop on all the data files
+# loop on all the data files
 for source in sourceFiles:
-    #open current data file for reading
+    # open current data file for reading
     fh = open(source, "r")
-    #loop on all lines
+    # loop on all lines
     for line in fh:
-        #check if it is a header
+        # check if it is a header
         if not 'distance' in line:
-            #read the whole file in
+            # read the whole file in
+            # MN: why do you build data to compute the number of lines when you can use person_distances?
             data.append(line.strip('\n'))
-            lines = sum(1 for lines in data)
+            # MN: why not using len(data)?
+            # MN: with this statement here you are computing the number of lines with every line read
+            #     you should move it outside this loop after you are done reading all the dat ain input
+            #lines = sum(1 for lines in data)
             line = line.strip('\n')
+            # MN: pay attention that key has a space appended at the end.
+            #     you might run into issues
             (key, val) = line.split(',')
             #if person appears more than once, appends run time to the end of the name
             if(key in person_distances):
                 person_distances[key].append(float(val))
             else:
                 person_distances[key] = [float(val)]
+            # MN: why don't you move this line above the previous if statment, so you can use val everywhere instead of float(val)?
             val = float(val)
-            #calculate max and min for all runners
+            # calculate max and min for all runners
+            # MN: computing your min and max here, you work on each single distance and not on the comulative distance for each person
             if(val > max):
                 max = val
                 maxkey = key
@@ -48,10 +66,15 @@ for source in sourceFiles:
     #close file
     fh.close()
 
-#strip end of line command from each distance value
+# MN: compute number of lines read here. Moved from within the previous loop
+lines = len(person_distances);
+
+# strip end of line command from each distance value
 distances = [item.strip('\n').split(',')[1] for item in data]
 
-#calculate sum of total distance
+# calculate sum of total distance
+# MN: you could have used distances that you defined right above
+#     total_distances = sum(distances)
 total_distance = sum(\
                     [float(item.strip('\n').split(',')[1])\
                      for item in data])
